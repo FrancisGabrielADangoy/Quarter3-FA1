81f4dc98c312dd12bc3c1b9b5325cbfba8268e8f
FrancisGabrielADangoy <francisgabriel.dangoy@smc.pshs.edu.ph>
2025-01-28 07:19:46 +0800

    added html and js files

---
+++ GFA1JS3.js
@@ -0,0 +1,5 @@
+var consent = confirm("Do you wish to share your personal information? ");
+if(consent)
+    console.log("Name: " +nickname+ "\nHeight: " +heightConv2+ "'" +heightConv1+ "\"" + "\nWeight: " +weightConv+ " lbs");
+else
+    console.log("User does not wish to share his/her information.");
